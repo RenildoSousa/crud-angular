85741ed6133d8d1975755e5f0430ec67b4aded8f
renildosv <@Mariaalves8>
2023-10-18 17:29:31 -0300

    add routing, toolbar and table

--- src/app/courses/courses-routing.module.ts
+++ src/app/courses/courses-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+
 import { CoursesComponent } from './courses/courses.component';
 
 const routes: Routes = [
@@ -7,7 +8,7 @@ const routes: Routes = [
 ];
 
 @NgModule({
-  imports: [ RouterModule.forChild(routes) ],
-  exports: [ RouterModule ]
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule]
 })
 export class CoursesRoutingModule { }
--- src/app/courses/courses/courses.component.ts
+++ src/app/courses/courses/courses.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Course } from '../model/course';
 
 @Component({
   selector: 'app-courses',
   templateUrl: './courses.component.html',
-  styleUrls: ['./courses.component.scss']
+  styleUrls: ['./courses.component.scss'],
 })
-export class CoursesComponent {
+export class CoursesComponent implements OnInit {
+  courses: Course[] = [
+    { _id: "1", nome: "Janio", categoria: "professor"},
+    { _id: "1", nome: "ROmildo", categoria: "professor"},
+    { _id: "1", nome: "Renildo", categoria: "Aluno"},
+    { _id: "1", nome: "Genildo", categoria: "professor"}
 
+  ];
+  displayedColumns = ['nome', 'categoria']
+
+  constructor() {}
+
+  ngOnInit(): void {
+    throw new Error('Method not implemented.');
+  }
 }
